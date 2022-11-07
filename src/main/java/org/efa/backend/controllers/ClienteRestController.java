389d591766de8220bf6701c11be74e5d966f8d79
Alfredo Garijo <31098698+AlfredoGari@users.noreply.github.com>
2022-11-07 20:08:57 -0300

    Update ClienteRestController.java

--- src/main/java/org/efa/backend/controllers/ClienteRestController.java
+++ src/main/java/org/efa/backend/controllers/ClienteRestController.java
@@ -1,4 +1,123 @@
-package org.efa.backend.controllers;
+import org.efa.backend.exceptions.custom.BusinessException;
+import org.efa.backend.exceptions.custom.FoundException;
+import org.efa.backend.exceptions.custom.NotFoundException;
+import org.efa.backend.miscellaneous.Paths;
+import org.springframework.transaction.annotation.Transactional;
+import org.efa.backend.model.Cliente;
+import org.efa.backend.model.business.IClienteBusiness;
+import org.efa.backend.utils.IStandardResponseBusiness;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(Paths.URL_CLIENTES)
 public class ClienteRestController {
+	
+	@Autowired
+    private IStandardResponseBusiness response;
+	
+	@Autowired
+    private IClienteBusiness clienteBusiness;
+	
+	@PostMapping(value = "/crear")
+    public ResponseEntity<?> load(@RequestBody Cliente cliente) {
+        try {
+            Cliente response = clienteBusiness.add(cliente);
+            HttpHeaders responseHeaders=new HttpHeaders();
+            responseHeaders.set("location",Paths.URL_CLIENTES+"/"+response.getId());
+            return new ResponseEntity<>( responseHeaders, HttpStatus.CREATED);
+        } catch (FoundException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.NOT_FOUND, e, e.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (BusinessException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+	
+	
+	@GetMapping(value="/listar", produces= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> loadAll() {
+        try {
+            return new ResponseEntity<>(clienteBusiness.loadAll(), HttpStatus.OK);
+        } catch (BusinessException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+	
+	@GetMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> loadById(@PathVariable("id") Long id) {
+        try {
+            return new ResponseEntity<>(clienteBusiness.loadById(id), HttpStatus.OK);
+        } catch (BusinessException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.NOT_FOUND, e, e.getMessage()), HttpStatus.NOT_FOUND);
+        }
+    }
+	
+	@GetMapping(value = "/{razonSocial}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> load(@PathVariable("razonSocial") String razonSocial) {
+        try {
+            return new ResponseEntity<>(clienteBusiness.load(razonSocial), HttpStatus.OK);
+        } catch (BusinessException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.NOT_FOUND, e, e.getMessage()), HttpStatus.NOT_FOUND);
+        }
+    }
+	
+	@PutMapping(value = "/modificar")
+    public ResponseEntity<?> update(@RequestBody Cliente cliente) {
+        try {
+            clienteBusiness.update(cliente);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (BusinessException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.NOT_FOUND, e, e.getMessage()), HttpStatus.NOT_FOUND);
+        }
+    }
+	
+	@Transactional
+    @DeleteMapping(value="/eliminar/{razonSocial}")
+    public ResponseEntity<?> delete(@PathVariable("razonSocial") String razonSocial){
+        try {
+            clienteBusiness.delete(razonSocial);
+            return new ResponseEntity<>( HttpStatus.OK);
+        } catch (BusinessException | NotFoundException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+	
+    @Transactional
+    @DeleteMapping(value="/eliminar/id/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable("id") Long id){
+        try {
+            clienteBusiness.deleteById(id);
+            return new ResponseEntity<>( HttpStatus.OK);
+        } catch (BusinessException | NotFoundException e) {
+            return new ResponseEntity<>(response.build(HttpStatus.INTERNAL_SERVER_ERROR, e, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+	
+	
+	
+	
 }
